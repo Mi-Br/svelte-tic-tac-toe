a06a52c0b78b6550309a77a0ebab6829d3ce50c0
Mike <michailbredichin@gmail.com>
2023-04-08 14:51:48 +0300

    tictac initiatl commit

---
+++ tic-tac-toe/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Square from "./lib/Square.svelte";
+
+  let squares = Array(9).fill(false);
+</script>
+
+<main>
+  {#each squares as square, index}
+    <Square Value={square} id={index} />
+  {/each}
+</main>
+
+<style>
+  .row {
+    display: flex;
+  }
+</style>
---
+++ tic-tac-toe/src/lib/Square.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let Value;
+  export let Id;
+</script>
+
+<div class="square" id={Id}>
+  {Value}
+</div>
+
+<style>
+  .square {
+    width: 80px;
+    height: 80px;
+    margin: 1px;
+    border: 1px solid white;
+    text-align: center;
+    padding: 1px;
+    font-size: 50px;
+  }
+</style>
